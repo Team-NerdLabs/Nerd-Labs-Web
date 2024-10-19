0332c196d5c715250fa2b0c1281010449711de8b
정삼복 <68502584+3boku@users.noreply.github.com>
2024-10-19 23:02:51 +0900

    중앙 정렬

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="h-full w-full bg-black grid place-items-center">
+        <div className="min-h-screen w-full bg-black grid place-items-center">
             <Image
                 width="500"
                 height="500"
                 src="/NerdLabs.png"
                 alt="Nerd Labs"
             />
-            <h1 className="text-5xl font-bold">to be continue...</h1>
+            <h1 className="text-5xl font-semibold text-white">
+                to be continue...
+            </h1>
         </div>
     );
 }
